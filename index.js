d2a136d414ecf572ec74519b26797e7a794885a3
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-07-04 17:35:36 -0300

    update depois do env

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const { Client } = require('pg')
+require("dotenv").config()
+
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+
 
 const app = express()
 
-app.get('/test-api', function(req, res) {
+const connectDB = async () => {
+    client.connect().then(() => {
+        console.log('Conexão Realizada')
+    }).catch((error) => {
+        console.error("Erro ao conectar", error)
+    })
+}
+connectDB()
+
+app.get('/test-api', function (req, res) {
     res.send('Nossa api ta funfando')
-} )
+})
 
 
 app.listen(8000)
@@ -27,21 +48,21 @@ app.listen(8000)
 
 
 
-function filterEven(arr){
+function filterEven(arr) {
     let filteredArray = []
     for (let i = 0; i < arr.length; i++) {
         let testedNum = arr[i]
-        if(testedNum %2 == 0){
+        if (testedNum % 2 == 0) {
             filteredArray.push(testedNum)
         }
     }
-    return(filteredArray)
+    return (filteredArray)
 }
 
-arr = [1,2,3,4,5,6,7,8,9,10]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 resultado = filterEven(arr)
 
-function filtrarPares(array){
-    return array.filter(numero => numero %2 == 0) // testando nova função array.prototype.filter()
+function filtrarPares(array) {
+    return array.filter(numero => numero % 2 == 0) // testando nova função array.prototype.filter()
 }
 
